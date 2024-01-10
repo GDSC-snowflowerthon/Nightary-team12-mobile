bdbaed5e053480b3f6fffab04edcf2ada69a37ad
HyungJoonSon <kr.dev.hyungjoon@gmail.com>
2024-01-11 04:28:03 +0900

    feat: Add Factory Instance Init

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,19 @@ import 'package:intl/date_symbol_data_local.dart';
 import 'package:nightary/main_app.dart';
 import 'package:nightary/utilities/app_routes.dart';
 
+import 'apps/factory/database_factory.dart';
+import 'apps/factory/shared_preference_factory.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   /* Open .env file */
   await dotenv.load(fileName: "assets/config/.env");
   await initializeDateFormatting();
+
+  /* Initialize Factory */
+  await SharedPreferenceFactory.init();
+  DatabaseFactory.init();
 
   // Splash Screen Duration 1.0s
   await Future.delayed(const Duration(seconds: 1));
