3a98e9a12102a4a2c857b0255a8e185ee235071d
HyungJoonSon <kr.dev.hyungjoon@gmail.com>
2024-01-11 02:35:58 +0900

    feat: Create Default Query In Dao

--- lib/domains/dao/sleep_record_dao.dart
+++ lib/domains/dao/sleep_record_dao.dart
@@ -4,11 +4,35 @@ import 'package:nightary/domains/entities/sleep_record.dart';
 
 part 'sleep_record_dao.g.dart';
 
-/// DataClass를 통해 toJson, fromJson을 자동으로 생성해줌
-/// SleepRecordData.fromJson(json) 형태로 사용 가능
+/// SleepRecordDao Class
+/// 1. DataBase Accessor Class
 ///
+/// SleepRecordData Class
+/// 1. 기본적인 Return Value
+/// 2. 정렬 및 비교 가능
+/// 3. SleepRecordData.fromJson(json) 형태로 사용 가능
+/// 4. SleepRecordData.toJson() 형태로 사용 가능
 @DriftAccessor(tables: [SleepRecord])
 abstract class SleepRecordDao extends DatabaseAccessor<NightaryDatabase>
     with _$SleepRecordDaoMixin {
   SleepRecordDao(super.db);
+
+  // Create
+  Future<void> save(SleepRecordCompanion entity) =>
+      into(sleepRecord).insert(entity);
+
+  // Read One
+  Future<List<SleepRecordData>> findById(int id) =>
+      (select(sleepRecord)..where((tbl) => tbl.id.equals(id))).get();
+
+  // Read All
+  Future<List<SleepRecordData>> findAll() => select(sleepRecord).get();
+
+  // Update
+  Future<void> updateByEntity(SleepRecordData entity) =>
+      update(sleepRecord).replace(entity);
+
+  // Delete
+  Future<void> deleteByEntity(SleepRecordData entity) =>
+      delete(sleepRecord).delete(entity);
 }
